b4aaaf5076a95b2dcb52824822a5bcc6fbc16636
Wagner-rosa-dev <wagner.rosa.dev@gmail.com>
2025-06-12 16:26:53 -0300

    implementação da logica de pacote de dados no microcontrolador

--- sketch_jun11a/sketch_jun11a.ino
+++ sketch_jun11a/sketch_jun11a.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+const int pinoPotenciometro = 35;
+const int pino2 = 32;
 
-}
+
+
+void setup() {
+  Serial.begin(115200);
+
+
+  Serial.println("Programa iniciado no ESP32. Girando o Potenciometro...");
+  
+  }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  int valorLido = analogRead(pinoPotenciometro);
+  int valor2 = analogRead(pino2);
+  
+  int valorMapeado = map(valorLido, 0, 4095, 0, 100);
+  int valorM2 = map(valor2, 0, 4095, 0, 10);
+
+  Serial.print("Velocidade: ");
+  Serial.print(valorM2);
+  Serial.print(" K/h");
+  Serial.print(" | ");  
+  Serial.print("Latitude: ");
+  Serial.println(valorMapeado);
+  
+
+  delay(100);
 
 }
